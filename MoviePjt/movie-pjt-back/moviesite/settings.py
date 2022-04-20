f70fc67f935b27becbbff468bf5803a5977bd220
양승우 <aquageek94@naver.com>
2022-04-21 00:55:39 +0900

    Django : moviesite/settings.py

--- MoviePjt/movie-pjt-back/moviesite/settings.py
+++ MoviePjt/movie-pjt-back/moviesite/settings.py
@@ -11,16 +11,25 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os, environ
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(
+    env_file = os.path.join(BASE_DIR, '.env')
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(v$*0pdl%p2%^hh#ku&vbtpunp5lx(c@!aurri+u+-m328tfp_'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
