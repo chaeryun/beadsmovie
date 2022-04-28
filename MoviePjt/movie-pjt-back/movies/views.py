147dfd06283bbe849dba308a7fe62c11dd0e1b29
김채련 <kimchaeryun@gmail.com>
2022-04-29 00:11:49 +0900

    Fix views.py

--- MoviePjt/movie-pjt-back/movies/views.py
+++ MoviePjt/movie-pjt-back/movies/views.py
@@ -9,3 +9,10 @@ from .models import MovieList
 from .serializers import MovieSerializer
 
 
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def main(request):
+    if request.method == 'GET':
+        movieslist = MovieList.objects.all()
+        serializer = MovieSerializer(movieslist, many=True)
+        return Response(serializer.data)
