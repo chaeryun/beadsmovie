bae2339c636f355a85fbf41035b4f7fa2298d173
김채련 <kimchaeryun@gmail.com>
2022-04-28 23:34:23 +0900

    Add movies

---
+++ MoviePjt/movie-pjt-back/movies/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+from .models import MovieList
+from .serializers import MovieSerializer
+
+
