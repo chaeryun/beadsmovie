2c5af441e868cabc83d9972fadd0b0293defbc3f
황이레 <gadkham@gmail.com>
2022-05-03 23:59:35 +0900

    FEAT : 소셜 로그인 JWT 발급

--- MoviePjt/movie-pjt-back/user/views.py
+++ MoviePjt/movie-pjt-back/user/views.py
@@ -1,7 +1,11 @@
+import json
 import requests
 from django.shortcuts import redirect, get_list_or_404
-from rest_framework.decorators import api_view
+from rest_framework_jwt.settings import api_settings
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from bson import json_util
 from .models import User
 from .serializers import UserSerializer
 
@@ -9,12 +13,14 @@ OAUTH_KAKAO_HOST = 'https://kauth.kakao.com'
 OAUTH_KAKAO_CLIENT_ID = '5835235e4bcc589a0bd165c4d85651a8'
 OAUTH_KAKAO_REDIRECTION_URL = 'http://localhost:8000/user/oauth/kakao/callback'
 
-@api_view(["POST"])
+@api_view(["GET"])
+@permission_classes((AllowAny,))
 def redirect_kakao_permission_code(request):
     response = redirect(f'{OAUTH_KAKAO_HOST}/oauth/authorize?client_id={OAUTH_KAKAO_CLIENT_ID}&redirect_uri={OAUTH_KAKAO_REDIRECTION_URL}&response_type=code')
     return response
 
 @api_view(["GET"])
+@permission_classes((AllowAny,))
 def kakao_login(request):
     payload = request.query_params
     if  not payload['code']:
@@ -46,10 +52,18 @@ def kakao_login(request):
     # TODO: 응답 에러 처리
     id = payload['id']
     email = payload['kakao_account']['email']
-    user, is_created = User.objects.get_or_create(id=id, email=email, token=ACCESS_TOKEN)
+    nickname = payload['properties']['nickname']
+    user, is_created = User.objects.get_or_create(kakao_id=id, email=email, username=nickname)
+    JWT_PAYLOAD_HANDLER = api_settings.JWT_PAYLOAD_HANDLER
+    JWT_ENCODE_HANDLER = api_settings.JWT_ENCODE_HANDLER
     serializer = UserSerializer(user)
+    print(user)
+    payload = JWT_PAYLOAD_HANDLER(user)
+    user_id = json_util.dumps(payload["user_id"])
+    payload["user_id"] = json.loads(user_id)['$oid']
+    jwt_token = JWT_ENCODE_HANDLER(payload)
 
-    return Response(data=serializer.data)
+    return Response(data=jwt_token)
     
 @api_view(["GET"])
 def kakao_logout(request):
