9701156014b67117d552b3c803849743aa74d5f5
YUN LEE <yyyyy01179@naver.com>
2022-05-19 02:32:29 +0900

    Frontend : Add 전체조회 페이지

--- MoviePjt/movie-pjt-front/src/views/AllMovieListView.vue
+++ MoviePjt/movie-pjt-front/src/views/AllMovieListView.vue
@@ -1,8 +1,73 @@
 <template>
   <div class="all">
-      <v-bottom-navigation
+
+     <div class="text-center">
+       <br />
+      
+      <v-btn class="genre_button" @click="clickgenre($event)" value="28">
+        액션
+      </v-btn>
+      <v-btn class="genre_button" @click="clickgenre($event)" value="12">
+        모험
+      </v-btn>
+      <v-btn class="genre_button" @click="clickgenre($event)" value="16">
+        애니메이션
+      </v-btn>
+      <v-btn class="genre_button" @click="clickgenre($event)" value="35">
+        코미디
+      </v-btn>
+      <v-btn class="genre_button" @click="clickgenre($event)" value="80">
+        범죄
+      </v-btn>
+      <v-btn class="genre_button" @click="clickgenre($event)" value="99">
+        다큐멘터리
+      </v-btn>
+     
+      <v-btn class="genre_button" @click="clickgenre($event)" value="18">
+        드라마
+      </v-btn>
+      <v-btn class="genre_button" @click="clickgenre($event)" value="10751">
+        가족
+      </v-btn>
+      <v-btn class="genre_button" @click="clickgenre($event)" value="14">
+        판타지
+      </v-btn>
+      <br/>
+      <v-btn class="genre_button" @click="clickgenre($event)" value="36">
+        역사
+      </v-btn>
+      <v-btn class="genre_button" @click="clickgenre($event)" value="27">
+        공포
+      </v-btn>
+      <v-btn class="genre_button" @click="clickgenre($event)" value="10402">
+        음악
+      </v-btn>
+      <v-btn class="genre_button" @click="clickgenre($event)" value="9648">
+        미스터리
+      </v-btn>
+      <v-btn class="genre_button" @click="clickgenre($event)" value="10749">
+        로맨스
+      </v-btn>
+      <v-btn class="genre_button" @click="clickgenre($event)" value="878">
+        SF
+      </v-btn>
+      <v-btn class="genre_button" @click="clickgenre($event)" value="10770">
+        TV영화
+      </v-btn>
+      <v-btn class="genre_button" @click="clickgenre($event)" value="53">
+        스릴러
+      </v-btn>
+      <v-btn class="genre_button" @click="clickgenre($event)" value="10752">
+        전쟁
+      </v-btn>
+      
+      
+
+
+      </div>
+      <!-- <v-bottom-navigation
           color="white"
-          width="450px"
+          width="300px"
           style="
             border-radius: 10px;
             background-color:cadetblue;
@@ -10,96 +75,58 @@
           "
         >
         <v-btn value="red" @click="typered">
-          <span class="fs-3">최신 개봉일순</span>
+          <span class="fs-1">최신 개봉일순</span>
         </v-btn>
 
         <v-btn value="white" @click="typewhite">
-          <span class="fs-3">인기순</span>
+          <span class="fs-1">오래된 순</span>
         </v-btn>
 
         <v-btn value="rose" @click="typerose">
-          <span class="fs-3">Rose</span>
+          <span class="fs-1">인기순</span>
         </v-btn>
 
-        <v-btn value="sparkling" @click="typesparkling">
-          <span class="fs-3">Sparkling</span>
-        </v-btn>
+      </v-bottom-navigation> -->
 
-        <v-btn value="port" @click="typeport">
-          <span class="fs-3">Port</span>
-        </v-btn>
-
-      </v-bottom-navigation>
-
-<template v-if="winelistIsEmpty">
+<template v-if="movielistIsEmpty">
         <div class="justify-center mt-15" style="display: flex">
           <img
-            src="../assets/no_movie.png"
-            style="width: 70px; height: 65px"
+            src="../assets/loading.gif"
+            style="width: 300px; height: 300px"
             alt="empty"
           />
-          <h2 class="ml-5 mt-3 mb-15 pb-15">
-            조회하신 영화가 없습니다. 다른 영화를 검색해주세요.
-          </h2>
         </div>
       </template>
       <v-row   v-else>
         <div
           class="col-xl-3 col-lg-4 col-md-6"
           :key="i"
-          v-for="(wine, i) in calData"
+          v-for="(movie, i) in calData"
         >
           <v-card
             class="mx-auto mt-10"
-            max-width="380"
+            max-width="430"
             style="
-              border-radius: 80px;
+              border-radius: 30px;
               height: 530px;
               color: gainsboro;
-              background-color: #232320;
+              background-color: darkslategrey;
               box-shadow: 0 0 10px grey;
             "
             hover
             outlined
           >
-            <v-btn
-              class="mt-7 ml-15"
-              text
-              icon
-              large
-              :color="
-                wine.color == 'white'
-                  ? 'green lighten-3'
-                  : wine.color == 'red'
-                  ? 'red'
-                  : wine.color == 'rose'
-                  ? 'red lighten-3'
-                  : wine.color == 'port'
-                  ? 'blue lighten-3'
-                  : 'purple lighten-2'
-              "
-            >
-              <v-icon>mdi-circle</v-icon> {{ wine.color }}
-            </v-btn>
+          <br />
             <v-img
-              :src="wine.image"
-              height="300"
+              :src="generalurl + movie.poster_path"
+              height="450"
               alt="No image"
               contain
-              @click="winedetail(wine.wine_id)"
+              @click="gomoviedetail(movie._id)"
             /><v-img />
-            <hr
-              style="border-width: 2px; border-color: pink; margin-bottom: -5px"
-            />
             <v-card-title class="justify-center"
-              >[{{ wine.winery }}]</v-card-title
+              ><h3>{{ movie.title }}</h3></v-card-title
             >
-            <v-card-text
-              class="text-center fs-5"
-              style="margin-top: -12px; color: gainsboro"
-              >{{ wine.wine }}<br />
-              <br />
-            </v-card-text>
             <div
               class="d-flex justify-content-between align-items-center"
             ></div>
@@ -107,14 +134,220 @@
         </div>
       </v-row>
       <v-row>
+        <v-col col="4"></v-col>
         <v-pagination
           v-model="currentPage"
           :length="numOfPages"
           :total-visible="10"
-          style="margin-top: 10px; margin-bottom: 10px"
+          style="margin-top: 10px; margin-bottom: 10px; align-items:center;"
         ></v-pagination>
+        <v-col col="4"></v-col>
       </v-row>
 
 
     </div>
 </template>
+
+<script>
+import http from "@/util/http-common";
+
+export default {
+  name: "Movie",
+  data: () => ({
+    recentlist: [],
+    allmovielist: [],
+    recentmovielist: [],
+    oldmovielist: [],
+    popularmovielist: [],
+    
+    templist: [],
+    generalurl: "https://www.themoviedb.org/t/p/w220_and_h330_face",
+
+    //검색기능
+    keyword: "",
+
+    //검색영화리스트
+    keywordlist: [],
+
+    currentPage: 1,
+    perPage: 20,
+
+    gerne: "",
+    genrelist: [],
+    
+
+  }),
+
+  created() {
+    // movie page 클릭시 함수호출
+    this.getMovieList();
+  },
+
+  computed: {
+    startOffset() {
+      return (this.currentPage - 1) * this.perPage;
+    },
+    endOffset() {
+      return this.startOffset + this.perPage;
+    },
+    numOfPages() {
+      return Math.ceil(this.recentlist.length / this.perPage);
+    },
+    calData() {
+      return this.recentlist.slice(this.startOffset, this.endOffset);
+    },
+
+    movielistIsEmpty() {
+      // console.log(this.recentlist.length);
+      return this.recentlist.length == 0;
+    },
+  },
+
+  methods: {
+    // 전체 영화 불러와서 movielist에 넣기
+    async getMovieList() {
+      await http({
+        method: "get",
+        url: "/movie/",
+      })
+        .then((res) => {
+          // console.log("movie list :", res);
+          this.movielist = res.data;
+          this.recentlist = this.movielist;
+          this.templist = res.data;
+
+          // console.log("movielist", this.movielist);
+          this.typemovie(res.data);
+      
+
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+
+  
+
+    // 상세페이지 이동
+    gomoviedetail(movie_id) {
+      this.$router
+        .push({ path: "/moviedetail", query: { _id: movie_id } })
+        .catch(() => {});
+      location.reload();
+    },
+
+    // genre button 클릭시 해당장르영화 가져오기
+    clickgenre(e) {
+      this.genre_id =
+        e.target.value === undefined ? e.currentTarget.value : e.target.value;
+
+      this.genreclick();
+    },
+
+    async genreclick() {
+      await http({
+        methods: "get",
+        url: "movie/genre/" + this.genre_id + "/",
+      })
+        .then((res) => {
+          // console.log(res);
+          this.genrelist = res.data;
+          this.recentlist = res.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+
+  typemovie(movie) {
+      // 배열초기화
+      this.adventurelist = [];
+      this.romancelist = [];
+      this.animationlist = [];
+      this.comedylist = [];
+      this.actionlist = [];
+      this.dramalist = [];
+      this.familylist = [];
+      this.fantasylist = [];
+      this.historylist = [];
+      this.horrorlist = [];
+      this.musiclist = [];
+      this.misterylist = [];
+      this.sflist = [];
+      this.tvlist = [];
+      this.thrillerlist = [];
+      this.warlist = [];
+      this.westlist = [];
+
+      for (let i = 0; i < movie.length; i++) {
+        if (movie[i].genre_id == "12") {
+          this.adventurelist.push(movie[i]);
+        } else if (movie[i].genre_id == "16") {
+          this.animationlist.push(movie[i]);
+        } else if (movie[i].genre_id == "35") {
+          this.comedylist.push(movie[i]);
+        } else if (movie[i].genre_id == "10749") {
+          this.romancelist.push(movie[i]);
+        } else if (movie[i].genre_id == "80") {
+          this.actionlist.push(movie[i]);
+        } else if (movie[i].genre_id == "18") {
+          this.dramalist.push(movie[i]);
+        } else if (movie[i].genre_id == "10751") {
+          this.familylist.push(movie[i]);
+        } else if (movie[i].genre_id == "14") {
+          this.fantasylist.push(movie[i]);
+        } else if (movie[i].genre_id == "36") {
+          this.historylist.push(movie[i]);
+        } else if (movie[i].genre_id == "27") {
+          this.horrorlist.push(movie[i]);
+        } else if (movie[i].genre_id == "10402") {
+          this.musiclist.push(movie[i]);
+        } else if (movie[i].genre_id == "9648") {
+          this.misterylist.push(movie[i]);
+        } else if (movie[i].genre_id == "878") {
+          this.sflist.push(movie[i]);
+        } else if (movie[i].genre_id == "10770") {
+          this.tvlist.push(movie[i]);
+        } else if (movie[i].genre_id == "53") {
+          this.thrillerlist.push(movie[i]);
+        } else if (movie[i].genre_id == "10752") {
+          this.warlist.push(movie[i]);
+        } else if (movie[i].genre_id == "37") {
+          this.westlist.push(movie[i]);
+        }
+
+      }
+    },
+
+}
+}
+
+</script>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&display=swap");
+@font-face {
+  font-family: "NewWaltDisney";
+  src: url(../fonts/NewWaltDisney.ttf) format("truetype");
+}
+@font-face {
+  font-family: 'NanumSquareBold';
+  src: url(../fonts/NanumSquareL.ttf) format('truetype');
+}
+
+.all{
+  font-family: 'NanumSquareBold';
+}
+
+.genre_button{
+  box-shadow: 0 0 10px #3b3b34;
+  color: white !important;
+  height: 60px !important;
+  border-radius: 40px;
+  margin: 12px;
+  background-color: darkslategrey !important; 
+  font-weight: bold !important; 
+  font-size:x-large !important;
+  padding: 20px !important;
+}
+
+</style>
